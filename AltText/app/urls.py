c6e688eaed9fe65e0655aed209bfafbfa4a2d72b
liudmila <savioxessgaming@gmail.com>
2024-05-29 00:59:28 +0530

    Signin, Logout created, home page(empty) added

--- AltText/app/urls.py
+++ AltText/app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import Signup
+from .views import Signup, Signin, Home, Logout
 
 urlpatterns = [
-    path('signup/', Signup.as_view(), name="signup")
+    path('signup/', Signup.as_view(), name="signup"),
+    path('signin/', Signin.as_view(), name="signin"),
+    path('logout/', Logout.as_view(), name="logout"),
+    path('', Home.as_view(), name="home")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- AltText/app/views.py
+++ AltText/app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 
 # Create your views here.
@@ -19,6 +21,33 @@ class Signup(View):
         
         user = User.objects.create_user(username, email, password)
 
-        print(user)
+        return redirect('/signin')
 
+class Signin(View):
+    def get(self, request):
+        return render(request, 'login.html')
+    
+    def post(self, request):
+        data = request.POST
+        username = data.get('username')
+        password = data.get('password')
+        
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+
+        return redirect('/signin')
+
+
+class Home(LoginRequiredMixin, View):
+    login_url='/signin'
+    redirect_field_name=''
+    def get(self, request):
+        return render(request, 'home.html')
+
+class Logout(View):
+    def get(self, request):
+        logout(request)
         return redirect('/signin')
